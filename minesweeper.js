19eaa8a6f5eae21698378d4bbdc8701ae408fe2c
Rohan Jain <80338187+Rohanjain96@users.noreply.github.com>
2023-07-27 12:03:32 +0530

    patch: made changes to ui

--- minesweeper.js
+++ minesweeper.js
@@ -1,12 +1,13 @@
+let timeOut;
 
-function getMinePositions(size,numberofmines){
+function getMinePositions(length, breadth, numberofmines) {
     const positions = [];
-    while(numberofmines>0){
-        const x = Math.floor(Math.random()*size);
-        const y = Math.floor(Math.random()*size);
+    while (numberofmines > 0) {
+        const x = Math.floor(Math.random() * breadth);
+        const y = Math.floor(Math.random() * length);
 
-        if(!positions.some((position)=>position.x===x && position.y===y)){
-            positions.push({x,y});
+        if (!positions.some((position) => position.x === x && position.y === y)) {
+            positions.push({ x, y });
             numberofmines--;
         }
     }
@@ -14,19 +15,30 @@ function getMinePositions(size,numberofmines){
     return positions;
 }
 
-export function createBoard(size, numberofmines) {
+function debounce(callback, delay) {
+    return function (...args) {
+        if (timeOut) clearTimeout(timeOut)
+        timeOut = setTimeout(() => {
+            callback(...args)
+        }, delay)
+    }
+}
+
+const debounceFunction = debounce(playSound,0)
+
+export function createBoard(length, breadth, numberofmines) {
     const board = [];
-    const minePositions = getMinePositions(size,numberofmines);
-    for (let row = 0; row < size; row++) {
+    const minePositions = getMinePositions(length, breadth, numberofmines);
+    for (let row = 0; row < breadth; row++) {
         const Row = [];
-        for (let col = 0; col < size; col++) {
-            const element =  document.createElement('div');
+        for (let col = 0; col < length; col++) {
+            const element = document.createElement('div');
             element.classList.add('hidden');
             const tile = {
                 element,
-                x:row,
-                y:col,
-                mine:minePositions.some((position)=>position.x===row && position.y===col),
+                x: row,
+                y: col,
+                mine: minePositions.some((position) => position.x === row && position.y === col),
                 get status() {
                     return this.element.classList.value
                 },
@@ -43,74 +55,83 @@ export function createBoard(size, numberofmines) {
     return board;
 }
 
-function adjacentTiles(board,tile) {
-    const tiles= [];
+function adjacentTiles(board, tile) {
+    const tiles = [];
     let offSetTile;
-    for(let offsetx = -1; offsetx <= 1; offsetx++) {
-        for(let offsety = -1; offsety <= 1; offsety++) {
-           offSetTile = board[tile.x+offsetx]?.[tile.y+offsety];
-           if(offSetTile) tiles.push(offSetTile);
+    for (let offsetx = -1; offsetx <= 1; offsetx++) {
+        for (let offsety = -1; offsety <= 1; offsety++) {
+            offSetTile = board[tile.x + offsetx]?.[tile.y + offsety];
+            if (offSetTile) tiles.push(offSetTile);
         }
     }
     return tiles;
 }
-export function markTile(tile){
-    if(tile.status != 'hidden'&& tile.status != 'marked')
-     return;
-    if(tile.status == "marked"){
+
+export function markTile(tile) {
+    if (tile.status != 'hidden' && tile.status != 'marked')
+        return;
+    if (tile.status == "marked") {
         tile.status = "hidden";
     }
     else
-    tile.status = "marked";
+        tile.status = "marked";
+    playSound("marked.wav")
 }
 
-export function revealTile(board,tile){ 
-    if(tile.status != 'hidden'&& tile.status != 'marked') return;
-    if(tile.status == 'marked')return;
+function playSound(fileName) {
+    let audio = new Audio(fileName);
+    audio.play()
+}
 
-    if(tile.mine){
+export function revealTile(board, tile) {
+    if (tile.status != 'hidden' && tile.status != 'marked') return;
+    if (tile.status == 'marked') return;
+
+    if (tile.mine) {
         tile.status = "mine"
+        debounceFunction("blast.mp3")
         return;
     }
-    
-    tile.status ="revealed";
-    const nearByTiles = adjacentTiles(board,tile);
+
+
+    tile.status = "revealed";
+    const nearByTiles = adjacentTiles(board, tile);
     const mines = nearByTiles.filter(tile => tile.mine);
-    if(mines.length==0){
-      nearByTiles.forEach((tile)=>revealTile(board,tile));
+    if (mines.length == 0) {
+        nearByTiles.forEach((tile) => revealTile(board, tile));
     }
-    else{
-    const number = document.createElement("h3");
-    number.innerText = mines.length
-    switch(mines.length){
-        case 1: number.style.color ="red"
-        break;
-        case 2: number.style.color ="blue"
-        break;
-        case 3: number.style.color ="green"
-        break;
-        case 4: number.style.color ="pink"
-        break;
-        case 5: number.style.color ="purple"
-        break;
-        case 6: number.style.color ="violet"
-        break;
+    else {
+        const number = document.createElement("h3");
+        number.innerText = mines.length
+        switch (mines.length) {
+            case 1: tile.status = tile.status + " " + "blue"
+                break;
+            case 2: tile.status = tile.status + " " + "yellow"
+                break;
+            case 3: tile.status = tile.status + " " + "pink"
+                break;
+            case 4: tile.status = tile.status + " " + "green"
+                break;
+            case 5: tile.status = tile.status + " " + "violet"
+                break;
+        }
+        tile.element.append(number);
     }
-     tile.element.append(number);
-    }
+    playSound('reveal.wav')
 }
 
-export function checkLose(tile){
-    if(tile.mine&&tile.status!="marked"){
+export function checkLose(tile) {
+    if (tile.mine && tile.status != "marked") {
         return true;
     }
 
     else return false;
 }
 
-export function checkWin(board){
+export function checkWin(board) {
     return board.every(row => {
-         return row.every(tile => {
-            return( tile.status == "revealed" || (tile.mine && (tile.status == "marked" || tile.status =="hidden")))})
+        return row.every(tile => {
+            return (tile.status == "revealed" || (tile.mine && (tile.status == "marked" || tile.status == "hidden")))
+        })
     })
 }
